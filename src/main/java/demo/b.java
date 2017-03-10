20c9868da6227874404e92532cfe600208dd8bfc
yanbo-y <web_yb@163.com>
2017-03-10 10:48:04 +0800

    ss

---
+++ src/main/java/demo/b.java
@@ -0,0 +1,20 @@
+package demo;
+
+import com.controller.a;
+import com.controller.c;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+public class b {
+    public static void main(String[] args) {
+        c c=new c();
+        c.bb();
+        a cc=new a();
+        cc.aa();
+
+
+
+    }
+
+}
