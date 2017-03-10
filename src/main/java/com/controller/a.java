6d9715adb7c6eeebdea715012915190778588c84
yanbo-y <web_yb@163.com>
2017-03-10 10:55:30 +0800

    ss

---
+++ src/main/java/com/controller/a.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+
+public class a {
+    int id=2;
+    private int idd=5;
+    public int iddd=6;
+   public    void  aa(){
+        System.out.println("aaa");
+    }
+
+    public int getIdd() {
+        return idd;
+    }
+
+    public void setIdd(int idd) {
+        this.idd = idd;
+    }
+}
---
+++ src/main/java/com/controller/c.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+/**
+ * Created by Administrator on 2017/3/10.
+ */
+public class c {
+    public static void main(String[] args) {
+        a  c= new a();
+
+
+
+    }
+  public   void  bb(){
+        System.out.println("bbbbbbbbbbbbcccccccccczzzzzzzzzcccc");
+    }
+}
